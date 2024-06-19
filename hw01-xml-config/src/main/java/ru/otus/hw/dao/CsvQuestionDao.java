17803c954888bd6e5f1a223a61d970c2ea0736a6
Lykov Gleb <g.lykov@krtech.ru>
2024-06-19 15:20:44 +0300

    checkstyle updated

--- hw01-xml-config/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
+++ hw01-xml-config/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -19,10 +19,6 @@ public class CsvQuestionDao implements QuestionDao {
 
     @Override
     public List<Question> findAll() {
-        // Использовать CsvToBean
-        // https://opencsv.sourceforge.net/#collection_based_bean_fields_one_to_many_mappings
-        // Использовать QuestionReadException
-        // Про ресурсы: https://mkyong.com/java/java-read-a-file-from-resources-folder/
         List<QuestionDto> result;
 
         try (Reader reader =
